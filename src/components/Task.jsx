61083b934c30bd0bb32958fcdf738ef167c98534
Douglas Lima <douglaslima13bezerra@gmail.com>
2024-02-01 11:23:45 -0300

    feat: mudança de estilo nas tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,8 +3,10 @@ import { FaRegCircleCheck, FaRegTrashCan } from 'react-icons/fa6';
 const Task = ({ title, description }) => {
     return (
         <div className='task'>
-            <p className='title'>{title}</p>
-            <p className='description'>{description}</p>
+            <div className='text'>
+                <p className='title'>{title}</p>
+                <p className='description'>{description}</p>
+            </div>
             <div className='btns'>
                 <button className='delete'>
                     <FaRegTrashCan />
