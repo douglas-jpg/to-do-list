756de192d9251be11809240346c82d99b2c0ff66
douglas lima <douglaslima13bezerra@gmail.com>
2025-03-02 15:37:11 -0300

    chore(frontend) instalar dependenciar e configurar arquivos iniciais

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className='w-screen h-screen bg-blue-50'>
+            <h1 className='text-5xl'>Hello World</h1>
+            <h2 className='text-2xl'>To do List</h2>
+        </div>
+    );
+}
+
+export default App;
