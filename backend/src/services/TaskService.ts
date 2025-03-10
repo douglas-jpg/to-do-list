ce8914a99322091074445a7c4a6d8becac27267c
douglas lima <douglaslima13bezerra@gmail.com>
2025-03-10 11:08:55 -0300

    feat(backend): adicionar UpdateTask no TaskService

--- backend/src/services/TaskService.ts
+++ backend/src/services/TaskService.ts
@@ -71,18 +71,68 @@ export class TaskService implements ITaskService {
         }
     }
 
-    deleteTask(id: string): Promise<ITask> {
+    async deleteTask(id: string): Promise<ITask> {
         try {
             if (!id) throw new Error('ID inválido');
 
             const task = this.taskRepository.getTaskById(id);
             if (!task) throw new Error('Tarefa não encontrada');
 
-            return this.taskRepository.deleteTask(id);
-
+            return await this.taskRepository.deleteTask(id);
         } catch (error) {
             throw new Error(
                 `Falha ao deletar tarefa: ${(error as Error).message}`
+            );
+        }
+    }
+
+    async updateTask(id: string, param: ICreateTaskParams): Promise<ITask> {
+        try {
+            if (!id) throw new Error('ID inválido');
+
+            if (
+                !param.title ||
+                typeof param.title !== 'string' ||
+                param.title.trim().length < 3 ||
+                param.title.trim().length > 100
+            ) {
+                throw new Error(
+                    'Titulo invalido: deve ter entre 3 e 100 caracteres'
+                );
+            }
+
+            if (
+                param.description &&
+                (typeof param.description !== 'string' ||
+                    param.description.trim().length > 500)
+            ) {
+                throw new Error(
+                    'Descricao invalida: deve ter no maximo 500 caracteres'
+                );
+            }
+
+            if (!param.priority || typeof param.priority !== 'string') {
+                throw new Error(
+                    'Prioridade invalida: deve ser um dos valores permitidos.'
+                );
+            }
+
+            const task = await this.taskRepository.getTaskById(id);
+            if (!task) throw new Error('Tarefa não encontrada');
+
+            const updateTask: Partial<ITask> = {
+                title: param.title.trim(),
+                description: param.description
+                    ? param.description.trim()
+                    : undefined,
+                priority: param.priority,
+                updatedAt: new Date(),
+            };
+
+            return await this.taskRepository.updateTask(id, updateTask);
+        } catch (error) {
+            throw new Error(
+                `Falha ao atualizar tarefa: ${(error as Error).message}`
             );
         }
     }
