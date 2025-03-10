fe925fbcde4d17951b726b082ee6b712a84ef79e
douglas lima <douglaslima13bezerra@gmail.com>
2025-03-10 19:38:12 -0300

    feat(frontend): adicionar componente empty

---
+++ frontend/src/components/Empty/Empty.tsx
@@ -0,0 +1,12 @@
+const Empty = () => {
+    return (
+        <li className='text-center py-6 text-gray-500'>
+            <p className='text-lg'>Sem Tarefa!</p>
+            <p className='text-sm mt-2'>
+                Clique no botão acima para criar sua primeira tarefa
+            </p>
+        </li>
+    );
+};
+
+export default Empty;
