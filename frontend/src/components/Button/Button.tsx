768e8d831568cd23ce6e8cc7cecc031397dc5482
douglas lima <douglaslima13bezerra@gmail.com>
2025-03-17 13:22:54 -0300

    Refactor(frontend): Alterar estilo do botão

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,11 +1,24 @@
-const Button = () => {
+import { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    primary: boolean;
+    text: string;
+    onClick?: () => void;
+}
+
+const Button = ({ primary, text, onClick, ...rest }: ButtonProps) => {
     return (
         <button
-            className='bg-blue-600 hover:bg-blue-700 text-white font-inter font-semibold py-2 px-4 rounded-md transition-colors duration-100 cursor-pointer'
+            className={`font-inter text-white font-semibold py-2 px-4 rounded transition-colors duration-100 cursor-pointer ${
+                primary
+                    ? 'bg-blue-600 hover:bg-blue-700'
+                    : 'bg-gray-400 hover:bg-gray-500'
+            }`}
             aria-label='Criar nova tarefa'
-            onClick={() => console.log('Criar tarefa')}
+            onClick={onClick}
+            {...rest}
         >
-            Criar tarefa
+            {text}
         </button>
     );
 };
